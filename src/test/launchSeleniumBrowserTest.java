36f59a98dd6803850219f6802123948c3127ed8a
Cajetan <cajy78@live.com>
2020-09-19 19:55:26 +0530

    browser launch coded and tested

--- src/test/launchSeleniumBrowserTest.java
+++ src/test/launchSeleniumBrowserTest.java
@@ -1,10 +1,13 @@
 package test;
 
+import selenium.SeleniumWebDriver;
+import org.openqa.selenium.WebDriver;
+
 public class launchSeleniumBrowserTest {
 
 	public static void main(String[] args)
 	{
-		
+		WebDriver driver = SeleniumWebDriver.fireFoxDriver();
+		driver.get("https://www.amazon.in");
 	}
-
 }
